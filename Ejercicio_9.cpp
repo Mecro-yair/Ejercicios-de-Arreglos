70f2c1d3f4e4330c0e080d292781919fe8d879ce
Yair292 <dluquep@unjbg.edu.pe>
2024-07-02 01:25:40 -0500

    Agregar Ejercicio_9

---
+++ Ejercicio_9.cpp
@@ -0,0 +1,18 @@
+/*
+9. Rotar las componentes de un arreglo una posición hacia la derecha
+    12345    51234
+*/
+#include <iostream>
+using namespace std;
+int main(){
+	int n=0;
+	string numero;
+	cout<<"Digite un numero:\n"; cin>>numero;
+	while(numero[n]!='\0'){
+		n++;
+	}
+	
+	for(int i=0;i<=n;i++){
+		cout<<numero[n-i];
+	}
+}
